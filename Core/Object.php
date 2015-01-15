91973a63af43d1c5663655166d86440448eadcf9
smith_000 <smith_000@win>
2015-01-15 22:27:52 +0100

    object

---
+++ Core/Object.php
@@ -0,0 +1,19 @@
+<?php
+namespace Core {
+    
+    class Object {
+        public function __get($name) {
+            $getter = 'get'.$name;
+            if(method_exists($this, $getter)){
+                return $this->$getter();
+            }
+            else{
+                $setter = 'set'.$name;
+                if(method_exists($this, $setter)){
+                    throw new Exception('Getting write-only property '.get_class($this).'->'.$name);
+                }
+            }
+        }
+    }
+    
+}
