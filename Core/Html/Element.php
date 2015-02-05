8ba4b21c7891e86fb8625b79a44bb36fa6c965e3
Sergei Kovalenko <sergei.kovalenko@aeriagames.com>
2015-02-05 17:37:51 +0100

    commit

--- Core/Html/Element.php
+++ Core/Html/Element.php
@@ -2,8 +2,33 @@
 
 namespace vendor1\Core\Html {
 	
-	class Element extends \DOMElement {
+	/**
+	 * @property Head $Head Page head
+	 * @property Body $Body Page body
+	 */
+	class Page extends \DOMDocument {
+		protected $Head = null;
+		protected $Body = null;
 		
+		protected function GetHead(){
+			return $this->Head;
+		}
+		
+		protected function GetBody(){
+			return $this->Body;
+		}
+	}
+	
+	class Element extends \DOMElement {
+		//
+	}
+	
+	class Head extends Element {
+		//
+	}
+	
+	class Body extends Element {
+		//
 	}
 	
 }
---
+++ backend/htdocs/test.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+?>
+<html>
+	<head>
+		<title>test</title>
+	</head>
+	<body>
+		<div>
+			<pre><?php
+			
+
+
+			?></pre>
+		</div>
+	</body>
+</html>
