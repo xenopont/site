4ee58b1328897e9f975afd8d85c88e1273189882
smith_000 <smith_000@win>
2015-02-11 21:23:02 +0100

    IPropertyOwner, Html\BaseElement (IPropertyOwner)

--- core/html/Element.php
+++ core/html/Element.php
@@ -2,6 +2,9 @@
  
 namespace vendor1\Core\Html {
 	
+	use vendor1\Core\IPropertyOwner;
+	use vendor1\Core\Exceptions;
+	
 	/**
 	 * @property Head $Head Page head
 	 * @property Body $Body Page body
@@ -19,16 +22,56 @@ namespace vendor1\Core\Html {
 		}
 	}
 	
-	class Element extends \DOMElement {
-		//
-	}
-	
 	class Head extends Element {
 		//
 	}
 	
 	class Body extends Element {
 		//
+	}
+	
+	class Element extends BaseElement {
+		//
+	}
+	
+	class BaseElement extends \DOMElement implements IPropertyOwner{
+		public static function GetterName($property) {
+			return 'Get'.$property;
+		}
+		
+		public static function SetterName($property) {
+			return 'Set'.$property;
+		}
+		
+		public function __get($name) {
+			$getter = self::GetterName($name);
+			if(method_exists($this, $getter)){
+				return $this->$getter();
+			}
+			else{
+				if(method_exists($this, self::SetterName($name))){
+					throw new Exceptions\InvalidCall('Reading write-only property '.get_class($this).'->'.$name);
+				}
+				else{
+					throw new Exceptions\UnknownProperty('Reading unknown property '.get_class($this).'->'.$name);
+				}
+			}
+		}
+
+		public function __set($name, $value) {
+			$setter = self::SetterName($name);
+			if(method_exists($this, $setter)){
+				return $this->$setter($value);
+			}
+			else{
+				if(method_exists($this, self::GetterName($name))){
+					throw new Exceptions\InvalidCall('Writing read-only property '.get_class($this).'->'.$name);
+				}
+				else{
+					throw new Exceptions\UnknownProperty('Writing unknown property '.get_class($this).'->'.$name);
+				}
+			}
+		}
 	}
 	
 }
