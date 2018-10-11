cf799a51c937e7db99b7a914ef2b5a4265c90ec4
Sergei Kovalenko <smith404@live.com>
2018-10-11 22:35:26 +0200

    hello world in DOM

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,7 +1,11 @@
-import Router from './lib/jsapp/router';
+import m from './lib/jsapp/markup'
+import Router from './lib/jsapp/router'
 
 const router = new Router()
 
 router.addRoute('/', () => { console.log('hello') }) // tslint:disable-line:no-console
+router.addRoute('/path1', () => {
+    document.body.appendChild(m.div('hello world!').toHtml())
+})
 
 export default router
