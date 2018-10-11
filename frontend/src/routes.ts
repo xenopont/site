6ae22d07eb3fe4df8b9ec4292e0116a81facb8cb
Sergei Kovalenko <smith404@live.com>
2018-10-11 22:57:23 +0200

    two pages app (with reload)

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -3,9 +3,23 @@ import Router from './lib/jsapp/router'
 
 const router = new Router()
 
-router.addRoute('/', () => { console.log('hello') }) // tslint:disable-line:no-console
-router.addRoute('/path1', () => {
-    document.body.appendChild(m.div('hello world!').toHtml())
+router
+.addRoute('/', () => {
+    document.body.appendChild(
+        m.div({ style: { padding: '4px', background: '#454545', color: '#f4f4f4' , border: '1px solid #c00' } },
+            m.a({ href: '/path1' }, 'Path 1')
+        )
+        .onCreate(() => { console.log('index DOM created') }) // tslint:disable-line:no-console
+        .toHtml()
+    )
+})
+.addRoute('/path1', () => {
+    document.body.appendChild(
+        m.div({ style: { padding: '4px', background: '#fcfc00', color: '#444', border: '1px solid #454545' } },
+            m.a({ href: '/' }, 'Index')
+        )
+        .toHtml()
+    )
 })
 
 export default router
