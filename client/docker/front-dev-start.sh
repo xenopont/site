52294ffd910264d2de7c57513de485bf20b3db8b
Sergei Kovalenko <sergei.kovalenko@aeriagames.com>
2019-12-30 12:17:23 +0100

    parcel instead of webpack

--- client/docker/front-dev-start.sh
+++ client/docker/front-dev-start.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env ash
 
 # write aliases:
+echo "alias build=\"npm run build\"" >> /root/.ashrc
 echo "alias install=\"npm install\"" >> /root/.ashrc
 echo "alias lint=\"npm run lint\"" >> /root/.ashrc
-echo "alias ts=\"npm run ts\"" >> /root/.ashrc
-echo "alias pack=\"npm run pack\"" >> /root/.ashrc
-echo "alias build=\"npm run build\"" >> /root/.ashrc
-echo "alias watch=\"npm run watch\"" >> /root/.ashrc
 #echo "alias test=\"npm run test\"" >> /root/.ashrc
+echo "alias watch=\"npm run watch\"" >> /root/.ashrc
 
 # apply aliases:
 . /root/.ashrc
 
 nginx -g "pid /tmp/nginx.pid; daemon off;"
-#npm run watch
