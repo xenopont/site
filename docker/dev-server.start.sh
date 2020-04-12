ed7f0ae6b6b08482f88420e65812c63fc958c7eb
Sergei Kovalenko <sergei.kovalenko@aeriagames.com>
2020-04-12 21:36:06 +0200

    server start script fix (black magic; might be line endings)

--- docker/dev-server.start.sh
+++ docker/dev-server.start.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env ash
-# write aliases:
-#echo "alias build=\"npm run build\"" >> /root/.ashrc
-#echo "alias install=\"npm install\"" >> /root/.ashrc
-#echo "alias lint=\"npm run lint\"" >> /root/.ashrc
-#echo "alias watch=\"npm run watch\"" >> /root/.ashrc
-# apply aliases:
-#. /root/.ashrc
+
 tail -f /dev/null
